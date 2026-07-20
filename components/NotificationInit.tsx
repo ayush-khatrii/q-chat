4d4b0b7af5aceed266470ee183692d8f2429818f
Ayush Khatri <108052277+ayush-khatrii@users.noreply.github.com>
2026-07-20 12:58:06 +0000

    add: logs for notifications

--- components/NotificationInit.tsx
+++ components/NotificationInit.tsx
@@ -3,7 +3,9 @@ import { useEffect } from "react";
 import { initFcm } from "@/lib/fcm";
 
 export default function NotificationInit({ userId }: { userId: string }) {
+
   useEffect(() => {
+    console.log("NotificationInit mounted for user", userId);
     initFcm(userId);
   }, [userId]);
 
